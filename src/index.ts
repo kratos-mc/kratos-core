fe3ce55e09db75cfbd1b981ad86853674feef038
Player Nguyên <daudaua3782@gmail.com>
2023-03-24 23:14:27 +0700

    docs: index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,26 @@ import * as download from "./download";
 import * as version from "./version";
 import * as utils from "./utils";
 import * as workspace from "./workspace";
-
+/**
+ * Represents kratos core object
+ */
 export const kratos = {
+  /**
+   * Represents the download module
+   *
+   */
   download,
+  /**
+   * Represents the version module
+   */
   version,
+  /**
+   * Represents the utility module
+   */
   utils,
+  /**
+   * Represents the workspace module
+   */
   workspace,
 };
 
