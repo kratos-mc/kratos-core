821ebb66b4b7e78ecc3e1a59abfa9d57abc5b78d
Player Nguyên <daudaua3782@gmail.com>
2023-03-31 09:02:17 +0700

    test: fix test output directory permission

--- src/test/utils/testOutput.ts
+++ src/test/utils/testOutput.ts
@@ -1,11 +1,13 @@
-import { emptyDir, remove } from "fs-extra";
+import { ensureDir, remove } from "fs-extra";
 
 export function getTestDirectoryPath() {
   return process.env.TEST_OUTPUT || "test-output";
 }
 
 export function makeTestDirectory(): Promise<void> {
-  return emptyDir(getTestDirectoryPath());
+  return ensureDir(getTestDirectoryPath(), {
+    mode: 0o2777,
+  });
 }
 
 export function removeTestDirectory(): Promise<void> {
